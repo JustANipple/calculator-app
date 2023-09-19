4a1410ab3a69c5a31c8423c4422146e99fd2637d
JustANipple <prandisamuel99@gmail.com>
2023-09-19 16:58:27 +0200

    modified:   src/App.jsx 	modified:   src/components/Calculator/Calculator.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
+import Calculator from './components/Calculator/Calculator'
+import ThemeSelector from './components/ThemeSelector/ThemeSelector'
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <main className="main">
+      <div className="top_bar">
+        <h1 className="title">calc</h1>
+        <ThemeSelector />
+      </div>
+      <Calculator />
+    </main>
   )
 }
 
--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -2,13 +2,52 @@ import styles from "./Calculator.module.css"
 
 const Calculator = () => {
 
+    function Display() {
+        return (
+            <div className={styles.display}>0</div>
+        )
+    }
+
+    function Keyboard() {
+        return (
+            <table>
+                <tr>
+                    <td> <button>7</button> </td>
+                    <td> <button>8</button> </td>
+                    <td> <button>9</button> </td>
+                    <td> <button>del</button> </td>
+                </tr>
+                <tr>
+                    <td> <button>4</button> </td>
+                    <td> <button>5</button> </td>
+                    <td> <button>6</button> </td>
+                    <td> <button>+</button> </td>
+                </tr>
+                <tr>
+                    <td> <button>1</button> </td>
+                    <td> <button>2</button> </td>
+                    <td> <button>3</button> </td>
+                    <td> <button>-</button> </td>
+                </tr>
+                <tr>
+                    <td> <button>.</button> </td>
+                    <td> <button>0</button> </td>
+                    <td> <button>/</button> </td>
+                    <td> <button>x</button> </td>
+                </tr>
+                <tr>
+                    <td> <button>reset</button> </td>
+                    <td> <button>=</button> </td>
+                </tr>
+            </table>
+        )
+    }
+
     return (
-        <main className={styles.main}>
-            <div className={styles.header}>
-                <h1 className={styles.title}>calc</h1>
-            </div>
-            
-        </main>
+        <div className={styles.calculator}>
+            <Display />
+            <Keyboard />
+        </div>
     )
 }
 

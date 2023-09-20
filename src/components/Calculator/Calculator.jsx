d4b10d24a1fd10c47fdd9bac792f996b7d7c0276
JustANipple <prandisamuel99@gmail.com>
2023-09-20 12:32:59 +0200

    modified:   src/App.css 	modified:   src/components/Calculator/Calculator.jsx 	modified:   src/components/Calculator/Calculator.module.css 	modified:   src/components/ThemeSelector/ThemeSelector.jsx 	modified:   src/components/ThemeSelector/ThemeSelector.module.css 	modified:   src/index.css

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -4,41 +4,43 @@ const Calculator = () => {
 
     function Display() {
         return (
-            <div className={styles.display}>0</div>
+            <div className={styles.display}>399,981</div>
         )
     }
 
     function Keyboard() {
         return (
-            <table>
-                <tr>
-                    <td> <button>7</button> </td>
-                    <td> <button>8</button> </td>
-                    <td> <button>9</button> </td>
-                    <td> <button>del</button> </td>
-                </tr>
-                <tr>
-                    <td> <button>4</button> </td>
-                    <td> <button>5</button> </td>
-                    <td> <button>6</button> </td>
-                    <td> <button>+</button> </td>
-                </tr>
-                <tr>
-                    <td> <button>1</button> </td>
-                    <td> <button>2</button> </td>
-                    <td> <button>3</button> </td>
-                    <td> <button>-</button> </td>
-                </tr>
-                <tr>
-                    <td> <button>.</button> </td>
-                    <td> <button>0</button> </td>
-                    <td> <button>/</button> </td>
-                    <td> <button>x</button> </td>
-                </tr>
-                <tr>
-                    <td> <button>reset</button> </td>
-                    <td> <button>=</button> </td>
-                </tr>
+            <table className={styles.keyboard}>
+                <tbody className={styles.keyboard_body}>
+                    <tr className={styles.table_row}>
+                        <td> <button>7</button> </td>
+                        <td> <button>8</button> </td>
+                        <td> <button>9</button> </td>
+                        <td> <button className={styles.del}>del</button> </td>
+                    </tr>
+                    <tr className={styles.table_row}>
+                        <td> <button>4</button> </td>
+                        <td> <button>5</button> </td>
+                        <td> <button>6</button> </td>
+                        <td> <button>+</button> </td>
+                    </tr>
+                    <tr className={styles.table_row}>
+                        <td> <button>1</button> </td>
+                        <td> <button>2</button> </td>
+                        <td> <button>3</button> </td>
+                        <td> <button>-</button> </td>
+                    </tr>
+                    <tr className={styles.table_row}>
+                        <td> <button>.</button> </td>
+                        <td> <button>0</button> </td>
+                        <td> <button>/</button> </td>
+                        <td> <button>x</button> </td>
+                    </tr>
+                    <tr className={styles.table_row_bottom}>
+                        <td className={styles.reset}> <button>reset</button> </td>
+                        <td className={styles.result}> <button>=</button> </td>
+                    </tr>
+                </tbody>
             </table>
         )
     }
--- src/components/ThemeSelector/ThemeSelector.jsx
+++ src/components/ThemeSelector/ThemeSelector.jsx
@@ -1,6 +1,23 @@
+import { useState } from "react"
 import styles from "./ThemeSelector.module.css"
 
 const ThemeSelector = () => {
+
+    const [knob, setKnob] = useState("start");
+
+    function handleClick() {
+        if(knob === "start") {
+            setKnob("center");
+            document.body.setAttribute("class", "theme-two");
+        } else if (knob === "center") {
+            setKnob("end");
+            document.body.setAttribute("class", "theme-three");
+        } else {
+            setKnob("start");
+            document.body.setAttribute("class", "");
+        }
+    }
+
     return (
         <div className={styles.theme_selector}>
 
@@ -12,8 +29,11 @@ const ThemeSelector = () => {
                     <li>2</li>
                     <li>3</li>
                 </ul>
-                <button className={styles.selector_switch}>
-                    <div className={styles.switch_knob}></div>
+                <button 
+                    className={styles.selector_switch}
+                    onClick={handleClick}
+                >
+                    <div className={`${styles.switch_knob} ${styles[knob]}`}></div>
                 </button>
             </div>
 
